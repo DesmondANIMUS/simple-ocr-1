123171df402cfccd405a0db6c282d54e2ead40fe
DesmondANIMUS <desmondmiles36@gmail.com>
2017-12-02 04:02:59 +0530

    init

---
+++ CV_demo_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using Emgu.CV;
+using Emgu.CV.OCR;
+using Emgu.CV.Structure;
+using System;
+
+namespace CV_demo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loading image....");
+            var image = new Image<Bgr, byte>("data.png");            
+            var tesOCRprovider = new Tesseract("./tessdata", "eng", OcrEngineMode.TesseractCubeCombined);
+
+            Console.WriteLine("Analysing data....");
+            tesOCRprovider.Recognize(image);
+            Tesseract.Character[] characters = tesOCRprovider.GetCharacters();
+
+            var text = tesOCRprovider.GetText();
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine(text);
+            Console.WriteLine("\n\nDone");
+            Console.ReadLine();
+        }
+    }
+}
